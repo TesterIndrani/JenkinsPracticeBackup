59bc72d0675000de02d49784fbeccef9e441c35b
TesterIndrani <glkindrani@gmail.com>
2023-04-29 11:01:49 +0530

    2nd commit

---
+++ src/test/java/practice/DemoTest.java
@@ -0,0 +1,13 @@
+package practice;
+import org.testng.annotations.Test;
+
+public class DemoTest
+	{
+		@Test
+		public void method()
+		{
+			System.out.println("method running");
+		}
+	}
+
+//1st push
